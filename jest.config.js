e3175c94891c54f611ee76dfaafde43e285e873b
Matt Berti <mat.berti@gmail.com>
2021-11-07 21:28:21 +0800

    Setup testing

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,32 @@
-// Sync object
-/** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-    roots: ['<rootDir>'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
-    moduleNameMapper: {
-        '^@/components(.*)$': '<rootDir>/components$1',
-        '^@/lib(.*)$': '<rootDir>/lib$1',
-        '^@/styles(.*)$': '<rootDir>/styles$1',
-        '\\.(scss|sass|css)$': 'identity-obj-proxy',
-        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
-    },
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
-    transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
-    transform: {
-        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    },
-    watchPlugins: [
-        'jest-watch-typeahead/filename',
-        'jest-watch-typeahead/testname',
-    ],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    /* Handle CSS imports (with CSS modules)
+    https://jestjs.io/docs/webpack#mocking-css-modules */
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+    /* Handle image imports
+    https://jestjs.io/docs/webpack#handling-static-assets */
+    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$':
+      '<rootDir>/__mocks__/fileMock.js',
+  },
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testEnvironment: 'jsdom',
+  transform: {
+    /* Use babel-jest to transpile tests with the next/babel preset
+    https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
 }
