dbb2f794a683a16feea34cc949ea6c4595f6e0c8
Matt Berti <mat.berti@gmail.com>
2022-02-19 19:19:18 +0800

    Update, Next 12, rm Babel (use Next compiler, Update Jest config

--- __tests__/snapshot.js
+++ __tests__/snapshot.js
@@ -3,10 +3,6 @@ import renderer from 'react-test-renderer'
 import Index from '../pages/index'
 import '@testing-library/jest-dom'
 
-test('two plus two is four', () => {
-  expect(2 + 2).toBe(4)
-})
-
 jest.mock('next/router', () => ({
   useRouter() {
     return {
