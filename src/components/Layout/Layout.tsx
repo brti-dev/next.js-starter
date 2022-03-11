9575f272bf4415eea129c300e29420d6667a820f
Matt Berti <mat.berti@gmail.com>
2022-03-11 13:12:39 +0800

    Cleanup

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -10,11 +10,13 @@ import {
 import { SkipNavLink, SkipNavContent } from '@reach/skip-nav'
 import '@reach/skip-nav/styles.css'
 
-import * as config from '../../../app.config.js'
+import config from '../../../package.json'
 import scrollToTop from 'lib/scroll-to-top'
 import useMediaQuery from 'lib/use-media-query'
 import { IconButton } from 'components/Button'
 import classes from './Layout.module.scss'
+
+const { pages, siteTitle } = config
 
 type OptionalChildren = {
   children?: React.ReactChild | React.ReactChild[]
@@ -36,9 +38,7 @@ export function Header({ children }: OptionalChildren) {
 
   const isScreenMobile = useMediaQuery('(max-width: 680px)')
 
-  const currentPageIndex = config.pages.findIndex(
-    page => page.link === pathnameRoot
-  )
+  const currentPageIndex = pages.findIndex(page => page.link === pathnameRoot)
 
   const [menu, setMenu] = useState(isScreenMobile)
   const handleOpenMenu = () => setMenu(!menu)
@@ -46,7 +46,7 @@ export function Header({ children }: OptionalChildren) {
   return (
     <header id="top" className={classes.header}>
       <div className="heading">
-        <h1>{config.siteTitle}</h1>
+        <h1>{siteTitle}</h1>
         {isScreenMobile && (
           <IconButton onClick={handleOpenMenu}>
             <MenuIcon />
@@ -63,7 +63,7 @@ export function Header({ children }: OptionalChildren) {
         aria-label="Main"
       >
         <ul>
-          {config.pages.map(({ link, title: pageTitle }) => (
+          {pages.map(({ link, title: pageTitle }) => (
             <li
               key={link}
               className={isCurrentPage(link) ? 'current' : undefined}
@@ -87,7 +87,7 @@ export function Footer({ children }: OptionalChildren) {
     <footer className={classes.footer}>
       <nav aria-label="Footer">
         <ul>
-          {config.pages.map(({ link, title: pageTitle }) => (
+          {pages.map(({ link, title: pageTitle }) => (
             <li key={link}>
               <Link href={link}>{pageTitle}</Link>
             </li>
@@ -102,10 +102,7 @@ export function Footer({ children }: OptionalChildren) {
   )
 }
 
-export default function Layout({
-  title = config.siteTitle,
-  children,
-}: LayoutProps) {
+export default function Layout({ title = siteTitle, children }: LayoutProps) {
   return (
     <>
       <Head>
@@ -120,10 +117,10 @@ export default function Layout({
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            config.siteTitle
+            siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
-        <meta name="og:title" content={config.siteTitle} />
+        <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <SkipNavLink />
