6fd66b02930689b027d2d99faaa94cbb693f85ca
Matt Berti <mat.berti@gmail.com>
2022-03-16 19:18:33 +0800

    Dialog label and onDismiss required props, fullscreen & closable prop

--- src/components/Dialog/dialog.tsx
+++ src/components/Dialog/dialog.tsx
@@ -1,27 +1,59 @@
 import { Dialog, DialogProps as ReachDialogProps } from '@reach/dialog'
 
 import useMediaQuery from 'lib/use-media-query'
+import { CloseButton } from './close-button'
 
-export type DialogProps = Omit<ReachDialogProps, 'isOpen'> & {
-  className?: string
+type DialogProps_base = Omit<ReachDialogProps, 'isOpen'> & {
+  // If true, add a CloseButton with onDismiss callback when clicked
+  closable?: boolean
   // Expand the modal to the edges of the viewport; 'auto' by default: fullscreen on mobile only
   fullscreen?: boolean | 'auto'
+  // Function called whenever the user hits "Escape" or clicks outside the dialog; Used to close the dialog or check if conditions are met before closing
   onDismiss: () => void // Required
-  open?: boolean // Renamed from isOpen for local lexical consistency
+  // Indicates if the dialog is open/shown
+  active?: boolean
 }
 
+type AriaLabel = {
+  'aria-label': string
+  'aria-labelled-by'?: never
+}
+
+type AriaLabelledBy = {
+  'aria-labelledby': string
+  'aria-label'?: never
+}
+
+export type DialogProps = DialogProps_base & (AriaLabelledBy | AriaLabel)
+
 export default function CustomDialog({
-  open,
+  active = false,
+  children,
+  closable = false,
   fullscreen = 'auto',
   ...rest
 }: DialogProps) {
   const isScreenMobile = useMediaQuery('(max-width: 640px)')
-  const isFullscreen = !fullscreen
-    ? false
-    : !!fullscreen
-    ? true
-    : isScreenMobile
-  console.log('fs', isFullscreen, fullscreen, isScreenMobile)
 
-  return <Dialog isOpen={open} data-fullscreen={isFullscreen} {...rest} />
+  let isFullscreen: boolean
+  if (!fullscreen) {
+    isFullscreen = false
+  } else {
+    if (fullscreen === true) {
+      isFullscreen = true
+    } else {
+      isFullscreen = isScreenMobile
+    }
+  }
+
+  return (
+    <Dialog
+      isOpen={active}
+      data-fullscreen={isFullscreen || undefined}
+      {...rest}
+    >
+      {closable && <CloseButton onClick={rest.onDismiss} />}
+      {children}
+    </Dialog>
+  )
 }
