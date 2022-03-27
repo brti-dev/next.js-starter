43763839f6d0372cb2d3cab998cb9d8d87514d7c
Matt Berti <mat.berti@gmail.com>
2022-03-27 20:17:55 +0800

    v2 remove ui components

--- pages/bar.tsx
+++ pages/bar.tsx
@@ -1,13 +1,9 @@
-import { Divider, Date } from 'matterial-ui'
+import Layout from 'components/Layout'
 
 export default function Bar() {
-  const date = '2000'
-
   return (
-    <>
-      <h1 style={{ color: 'var(--color-primary' }}>foo</h1>
-      <Divider style={{ margin: '1em 0' }} />
-      <Date date={date} />
-    </>
+    <Layout>
+      <h2>Bar</h2>
+    </Layout>
   )
 }
